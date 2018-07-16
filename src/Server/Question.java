8e31841163f6b04779b74a0ab11ca60c04aa2e55
oke-boy <32695442+oke-boy@users.noreply.github.com>
2018-07-16 22:34:21 +0900

    Update Question.java

--- src/Server/Question.java
+++ src/Server/Question.java
@@ -6,28 +6,31 @@ import java.util.ArrayList;
 
 public class Question implements Serializable {
 
-	
+	private static final long serialVersionUID = 6L;
+
 	private User questioner;		//質問者
 
 	private User answerer;		//回答者
 
 	private User offer;			//オファー人
 
+	private boolean offered;		//オファーされたかどうか
+
 	private String question;		//質問内容
 
-	private String answer;			//回答内容
+	private String answer = "";			//回答内容
 
 	private String group;			//質問の属するグループ
 
 	private ArrayList<User> candidates= new ArrayList<User>();;		//立候補者のリスト
 
-	private boolean answered = false;		//回答されたかどうか
+	private boolean answered;		//回答されたかどうか
 
 	private double value=0;				//回答の評価値
 
 	private int coin;				//質問にかけられたコイン
-	
-	
+
+
 
 
 
@@ -39,8 +42,12 @@ public class Question implements Serializable {
 		this.question = question;
 
 		this.group = group;
-		
+
 		this.coin=coin;
+
+		offered = false;
+
+		answered = false;
 
 	}
 
@@ -51,37 +58,43 @@ public class Question implements Serializable {
 		candidates.add(candidate);
 
 	}
-	
+
 
 
 
 	public void setOffer(User s) {
 
 		offer = s;
-		
+
+		offered = true;
 
 	}
 	public void canselOffer() {
 		offer=null;
+
+		offered = false;
 	}
+
 	public User getOffer() {
 		return offer;
 	}
-	
 
+	public boolean checkOffered() {
+		return offered;
+	}
 
 
 	public void setAnswerer(User user) {//回答し終わった人のこと
 
 		answerer = user;
 
-		
+
 
 	}
 	public void setAnswer(String s) {
 		answer=s;
 		answered=true;
-		
+
 	}
 
 
@@ -148,17 +161,9 @@ public class Question implements Serializable {
 		return candidates;
 
 	}
-	
+
 	public void clearCandidates() {
 		candidates.clear();
-	}
-
-
-
-	public User getOffered() {
-
-		return offer;
-
 	}
 
 
